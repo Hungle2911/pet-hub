3bbaf5dd94612c86441cb473348435a7e4034cc0
Tara <hymerstara@gmail.com>
2024-07-27 21:47:22 -0600

    "fix: updated to match current code"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      black: "#0a0908",
+      red: "#cc2936",
+      orange: "#F4A261",
+      darkOrange: "#E76F51",
+      beige: "#f7b267",
+      white: "#ffffff",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
-
