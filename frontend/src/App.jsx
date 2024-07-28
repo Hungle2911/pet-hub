efdf634ea3c9433b525f17996c9450cfb0a21097
Tara <hymerstara@gmail.com>
2024-07-27 21:48:47 -0600

    "Create: set up app.jsx, added imports"

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import HomePage from "./routes/Homepage";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import OwnerPage from "./components/Dashboard/OwnerPage";
+import SitterPage from "./components/Dashboard/SitterPage";
+import OwnerProfile from "./components/Profiles/OwnerProfile";
+import SitterProfile from "./components/Profiles/SitterProfile";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/owner-page" element={<OwnerPage />} />
+          <Route path="/sitter-page" element={<SitterPage />} />
+          <Route path="/owner-profile" element={<OwnerProfile />} />
+          <Route path="/sitter-profile" element={<SitterProfile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
