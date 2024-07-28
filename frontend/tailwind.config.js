de80987a166b79abc293a7080f833f1cbf46ab8a
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-07-27 22:47:25 -0400

    feat: set up page router

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'black': '#0a0908',
+      'red': '#cc2936',
+      'orange': '#F4A261',
+      'dark-orange': '#E76F51',
+      'beige': '#f7b267',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 };
