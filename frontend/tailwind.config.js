864faf6feb5fec3b3137bc07f0d529bedd98a73c
Tara <hymerstara@gmail.com>
2024-07-30 16:17:18 -0600

    "Fix: Updated colors"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      'black': '#0a0908',
-      'red': '#cc2936',
-      'orange': '#F4A261',
-      'dark-orange': '#E76F51',
-      'beige': '#f7b267',
-      'white': '#ffffff',
+      black: "#0a0908",
+      red: "#0b0575",
+      orange: "#130ade",
+      "dark-orange": "#EC86B4",
+      beige: "#f7b267",
+      white: "#ffffff",
     },
     fontFamily: {
-      playwrite: ['"Playwrite BE VLG"', 'sans-serif'],
+      playwrite: ['"Playwrite BE VLG"', "sans-serif"],
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '144': '36rem',
+        128: "32rem",
+        144: "36rem",
       },
       borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
