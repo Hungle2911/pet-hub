e24732c649a184d7d3d7fbb434239088e4687218
Tara <hymerstara@gmail.com>
2024-08-02 15:22:52 -0600

    "Fix:Update navbar and routes"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import Homepage from "./routes/Homepage";
 import Logout from "./components/Auth/Logout";
 import UserInfoForm from "./components/Profiles/UserInfoForm";
-import Homepage from "./routes/HomePage"
+// import OwnerPage from "./components/Dashboard/OwnerPage";
+// import SitterPage from "./components/Dashboard/SitterPage";
+// import OwnerProfile from "./components/Profiles/OwnerProfile";
+// import SitterProfile from "./components/Profiles/SitterProfile";
+// import CatInfoForm from "./components/Profiles/PetInfoForm";
 import "./App.css";
+
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Logout />} />
+          {/* <Route path="/owner-page" element={<OwnerPage />} />
+          <Route path="/sitter-page" element={<SitterPage />} />
+          <Route path="/owner-profile" element={<UserInfoForm />} />
+          <Route path="/owner-profile" element={<OwnerProfile />} />
+          <Route path="/sitter-profile" element={<SitterProfile />} /> 
+          <Route path="/petinfo" element={<petInfoForm />} />*/}
         </Routes>
       </main>
     </div>
