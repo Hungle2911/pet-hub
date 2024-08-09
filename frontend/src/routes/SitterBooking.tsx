03ae0331f648aa7bdf8289dbc3a065f21b55974e
Tara <hymerstara@gmail.com>
2024-08-09 01:12:34 -0600

    "styling: updated styling to match other pages"

--- frontend/src/routes/SitterBooking.tsx
+++ frontend/src/routes/SitterBooking.tsx
@@ -10,6 +10,7 @@ const SitterBooking = () => {
   const [sitterInfo, setSitterInfo] = useState<CatSitter>();
   const [showModal, setShowModal] = useState<boolean>(false);
   const { isAuthenticated, loginWithRedirect } = useAuth0();
+
   const getSitterInfo = async () => {
     try {
       const response = await api.get(`/cat-sitters/profile/${sitterId}`);
@@ -40,23 +41,35 @@ const SitterBooking = () => {
           sitterInfo={sitterInfo!}
         />
       )}
-      <div className="container mx-auto px-6 py-4">
-        <h2 className="text-2xl font-bold">Cat Sitter Profile</h2>
-        <span className="text-lg">
-          {sitterInfo?.user.first_name} {sitterInfo?.user.last_name}
-        </span>
-        <p className="text-lg">Address: {sitterInfo?.user.location}</p>
-        <p className="text-lg">Description:</p>
-        <span className="text-lg">{sitterInfo?.user.description}</span>
-        <p className="text-lg">Experience: {sitterInfo?.experience}</p>
-        <p className="text-lg">Rates: ${sitterInfo?.rate}/night</p>
-        {/* <p className="text-lg">Reviews: 4.9 stars</p> */}
-        <button
-          className="w-full bg-dark-orange text-white p-2 rounded hover:bg-blue-600"
-          onClick={onClick}
-        >
-          Book an appointment
-        </button>
+      <div className="max-w-2xl mx-auto mt-8 p-6 bg-pink-100 rounded-lg shadow-lg border-2 border-pink-400">
+        <h2 className="text-2xl font-bold mb-6 text-custom-blue-500">
+          Cat Sitter Profile
+        </h2>
+        <div className="space-y-4">
+          <span className="block text-lg font-bold text-gray-700">
+            {sitterInfo?.user.first_name} {sitterInfo?.user.last_name}
+          </span>
+          <p className="text-lg text-gray-700">
+            Address: {sitterInfo?.user.location}
+          </p>
+          <p className="text-lg text-gray-700 font-bold">Description:</p>
+          <span className="block text-lg text-gray-700">
+            {sitterInfo?.user.description}
+          </span>
+          <p className="text-lg text-gray-700">
+            Experience: {sitterInfo?.experience}
+          </p>
+          <p className="text-lg text-gray-700">
+            Rates: ${sitterInfo?.rate}/night
+          </p>
+          {/* <p className="text-lg">Reviews: 4.9 stars</p> */}
+          <button
+            className="w-full bg-transparent text-pink py-2 px-4 rounded-full border-2 border-pink hover:bg-pink hover:text-white transition-transform transform hover:scale-105 shadow-lg"
+            onClick={onClick}
+          >
+            Book an appointment
+          </button>
+        </div>
       </div>
     </>
   );
