9e162df8b3698f2a4c6cabad35c9cfb9346b69fa
Tara <hymerstara@gmail.com>
2024-08-09 01:10:12 -0600

    "fix: updated code to match latest merges"

--- frontend/src/components/Auth/LoginButton.tsx
+++ frontend/src/components/Auth/LoginButton.tsx
@@ -11,8 +11,7 @@ const LoginButton = () => {
   // };
   return (
     <button
-      onClick={handleLogin}
-      //onClick={() => loginWithRedirect()}
+      onClick={() => loginWithRedirect()}
       className="bg-off-white text-grey py-1 px-2 md:py-2 md:px-4 rounded border border-grey-500 text-sm md:text-base"
     >
       Login
