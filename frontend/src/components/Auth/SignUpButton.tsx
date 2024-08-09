464017caedd3c48616ba85d972a03bf8aa3663c4
Tara <hymerstara@gmail.com>
2024-08-09 01:10:37 -0600

    "fix: linked button to sign up page"

--- frontend/src/components/Auth/SignUpButton.tsx
+++ frontend/src/components/Auth/SignUpButton.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const SignUpButton = () => {
+  const navigate = useNavigate();
+
+    const handleSignUp = () => {
+      navigate("/sign-up");
+    };
+
   return (
-    <button className="bg-off-white text-grey py-1 px-2 md:py-2 md:px-4 rounded border border-grey-500 text-sm md:text-base">
+    <button
+      onClick={handleSignUp}
+      className="bg-off-white text-grey py-1 px-2 md:py-2 md:px-4 rounded border border-grey-500 text-sm md:text-base"
+    >
       Sign Up
     </button>
   );
