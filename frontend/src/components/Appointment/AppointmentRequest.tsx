d65a5397820e71eeb506795cbfd84c33dc7b85d7
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-08-10 18:31:05 -0400

    style: add styling to request appointment list

--- frontend/src/components/Appointment/AppointmentRequest.tsx
+++ frontend/src/components/Appointment/AppointmentRequest.tsx
@@ -3,19 +3,44 @@ import { formatDate } from "../../utilities/date_converter";
 const AppointmentRequest = ({ data }: any) => {
   return (
     <>
-      <div>Appointment Request</div>
-      <ul>
+      <div className="text-xl font-semibold mb-4">Appointment Request</div>
+      <ul className="space-y-4">
         {data.map((appointment) => (
-          <li key={appointment.id} className="border-2 my-2 border-orange">
+          <li
+            key={appointment.id}
+            className="border-2 p-4 rounded-lg border-orange bg-white shadow-sm"
+          >
             <div>
-              Requester: {appointment.catOwner.user.first_name}{" "}
-              {appointment.catOwner.user.last_name}
+              Requester:{" "}
+              <span className="font-semibold">
+                {appointment.catOwner.user.first_name}{" "}
+                {appointment.catOwner.user.last_name}
+              </span>
             </div>
-            <div>Drop-off: {formatDate(appointment.startDate)}</div>
-            <div>Pick-up: {formatDate(appointment.endDate)}</div>
-            <div>Status: {appointment.status}</div>
-            <button>Confirm</button>
-            <button>Reject</button>
+            <div>
+              Drop-off:{" "}
+              <span className="font-semibold">
+                {formatDate(appointment.startDate)}
+              </span>
+            </div>
+            <div>
+              Pick-up:{" "}
+              <span className="font-semibold">
+                {formatDate(appointment.endDate)}
+              </span>
+            </div>
+            <div>
+              Status:{" "}
+              <span className="font-semibold">{appointment.status}</span>
+            </div>
+            <div className="mt-4 flex space-x-2">
+              <button className="px-4 py-2 bg-beige text-white rounded-lg hover:bg-green-600 transition">
+                Confirm
+              </button>
+              <button className="px-4 py-2 bg-red text-white rounded-lg hover:bg-red-600 transition">
+                Reject
+              </button>
+            </div>
           </li>
         ))}
       </ul>
